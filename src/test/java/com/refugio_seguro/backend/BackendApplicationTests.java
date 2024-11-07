2e727ca280502960e72b9b2341f3872303d14491
Ignacio Benito Herrero <ibenitoherrero@gmail.com>
2024-11-07 10:56:05 +0100

    First commit

---
+++ src/test/java/com/refugio_seguro/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.refugio_seguro.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
